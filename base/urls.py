25b87f64fa520c76583b602e5cc236704878b08d
OmidAfzali02 <dev.omid02@gmail.com>
2024-05-27 17:08:17 +0330

    pre-templates added

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,10 @@ app_name = 'base'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('login/', login, name='login'),
+    # path('signup/', login, name='signup'),
+    # path('send/', login, name='send'),
+    # path('track/', login, name='track'),
+    # path('agent_signup/', login, name='agent_signup'),
     
 ]
