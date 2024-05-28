fe4efacc221377c4a28ec3649a7e6e4e8f572a51
OmidAfzali02 <dev.omid02@gmail.com>
2024-05-28 08:45:19 +0330

    created and updated added to package model
    to know the time which a package was received or changed it's location

--- base/models.py
+++ base/models.py
@@ -67,6 +67,8 @@ delivery_choices = (('At agency', 'At agency'), ('At customer home', 'At custome
 
 class package(models.Model):
     id = models.UUIDField(primary_key=True ,verbose_name='package id', default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True) 
+    updated = models.DateTimeField(auto_now=True) 
     qr_code = models.ImageField(blank=True, null=True, upload_to='media/packages')
     contents = models.CharField(max_length=100, blank=True, default=str)
     length = models.PositiveSmallIntegerField(blank=True, default=0, verbose_name="Package length in cm")
